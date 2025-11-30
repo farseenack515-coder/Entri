a05cea2934fd5d36a04c13978785e3ae62592cef
farseenack515-coder <farseenack515@gmail.com>
2025-11-30 21:22:13 +0530

    Updated user name option in hello.c
    updated as per suggestion

--- hello.c
+++ hello.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 int main()
 {
+    char name[20];
+    
     printf("welcome to the world ");
-    printf("\nBegin your new journey");
+    printf("\nBegin your new journey.\n\n");
+    //user input
+    printf("Enter your name: ");
+    scanf("%s",name);
+    
 return 0;
 
 }
